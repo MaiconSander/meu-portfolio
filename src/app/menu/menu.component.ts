26807643bd947a0443abab7cf1bd5028c98d5695
Maicon <maiconvinsmoke@gmail.com>
2024-11-10 10:50:09 -0300

    menu do topo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
   constructor (private MenuService: MenuService) { }
+
+  menuStatus: string = 'closed';
+
+   menuItems?: Array<Menu>;
+
+  ngOnInit() {
+    const menu = this.MenuService.getItems();
+
+    this.menuItems = menu;
+  }
+
+  menuClick() {
+    //mecanismo de abrir e fechar!
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+
+  }
 
 }
